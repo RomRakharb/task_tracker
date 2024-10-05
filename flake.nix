bf40f7e197699429bd7c2187cf4b10ff3978605e
Rom Rakharb <rom.rakharb@mailfence.com>
2024-10-05 08:32:50 +0700

    first commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Simple rust dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+
+    devShells.x86_64-linux.default = pkgs.mkShell {
+
+      shellHook = ''
+        nu
+        exit
+      '';
+
+      packages = with pkgs; [
+        rustc
+        cargo
+        clippy
+        rustfmt
+        rust-analyzer
+        bacon
+      ];
+
+    };
+
+  };
+}
